9777841d4fbaec8b03383af4bf8d629b0dc23699
Ashiq Arib Gazi <Ashiqarib@gmail.com>
2025-03-11 19:48:03 -0400

    BASE SETUP

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Home, Menu, Search } from '@mui/icons-material';
+
+function App() {
+  return (
+    <div className="App" style={{ textAlign: 'center', padding: '20px' }}>
+      Hello
+      <Home />
+      <Menu />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
